6d1cb571948d48d156e0b610c8d7a68ac5f74168
Luca Maglie <luca.maglie2@gmail.com>
2020-07-14 14:59:24 +0200

    tentativi di ricerca nome in lista contatti.

--- script.js
+++ script.js
@@ -3,13 +3,71 @@ $(document).ready(function () {
 var target = $("#chat-utente");
 target.keyup(function () {
   var input = $(this);
-  var txt = $(".template p").text(input.val());
-  console.log("txt", txt);
+  var txt = $(".template .messaggio-utente p").text(input.val());
 })
 
 $(".fa-paper-plane").click(function () {
   var messaggioUtente = $(".template .box-messaggio-utente").clone();
   $(".chat").append(messaggioUtente);
   $("#chat-utente").val("");
+  setTimeout(rispostaAutomaticaSms, 1000);
 });
+
+function rispostaAutomaticaSms() {
+  var txt = $(".template .messaggio-interlocutore p").text("ok");
+  var messaggioInterlocutore = $(".template .box-messaggio-interlocutore").clone();
+  $(".chat").append(messaggioInterlocutore);
+}
+addSearchListener();
+searchContactName();
+function addSearchListener() {
+  var target = $("#searchbar");
+  target.keyup(sendKeyup);
+}
+
+function sendKeyup(event) {
+  var input = $(this);
+  var txt = input.val();
+  searchContactName(txt);
+}
+
+function searchContactName(txt) {
+  var target = $(".box-contatto .title");
+  if (target == txt) {
+    console.log(true);
+  }
+}
 });
+
+//  codice visto in classe con Giovanni
+
+// function addSendListener() {
+// var target = $("new-meessage-input"),
+// target.keyup(sendKeyup);
+// }
+
+// function sendKeyup(event) {
+  // var key = event.which;
+  // acquisisco il valore scritto nella stringa al press di invio(tasto 13)
+  // if (key === 13) {
+  //    var input = $(this);
+  //    var txt = input.val();
+  //    // resetto l'input
+  //    input.val("");
+    // sendMessage(txt);
+  // }
+// }
+
+// function sendMessage(txt) {
+//   var template = $(".template .box-messaggio-utente"").clone();
+//   var target = $("messaggio");
+//   template.find("#message-text").text(txt);
+//    template.find(#message-time).text(getActualTime());
+//   target.append(template);
+// }
+
+
+  // function getActualTime() {
+  //   var date = new Date();
+  //   return date.getHours() + ":" + date.getMinutes();
+  // }
